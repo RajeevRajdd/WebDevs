aa2ef69fc54a113b33131f0e27f7e25c20454718
RajeevRaj <rajeevmehara15320@gmail.com>
2025-08-30 15:38:41 +0530

    Variable in js

---
+++ HTMLFile/variablejs/variable.js
@@ -0,0 +1,30 @@
+a=10;
+console.log(a);
+console.log(typeof(a));
+
+age=24;
+name  = "Rajeev Kumar";
+console.log(age,name);
+console.log("type of age ", typeof(age));
+console.log("type of name",typeof(name));
+
+b=56.87;
+console.log("b = ",b ,"type of b ", typeof(b));
+
+
+c=16;
+console.log("C = ",c ,"type of c" ,typeof(c));
+
+price=99.99;
+console.log("price = ",price , "type of price ", typeof(price));
+
+displacement=-25
+console.log("displacement = ",displacement, "type of displacement ",typeof(displacement));
+
+
+a=10;
+b=5;
+console.log("a+b",a+b);
+console.log("a-b",a-b);
+console.log("a*b",a*b);
+console.log("a/b".a/b);
