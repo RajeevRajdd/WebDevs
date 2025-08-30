2a4bf370e48cc27d62855d3afd05b8e0bec7b0ed
RajeevRaj <rajeevmehara15320@gmail.com>
2025-08-30 15:47:08 +0530

    Arithmetic operator

---
+++ HTMLFile/operatorjs/arithmeticopr.js
@@ -0,0 +1,12 @@
+a=10;
+b=20;
+
+console.log("a=",a,"b=",b);
+
+console.log("Addition a and b : ",a+b);
+console.log("Substraction of a and b : ",a-b);
+console.log("Multiplication of a and b : ",a*b);
+console.log("Divide of a and b : ", a/b);
+console.log("Modulo of b and a : ",b%a);
+console.log("Exponentiation of a and b : ",a**b);
+
